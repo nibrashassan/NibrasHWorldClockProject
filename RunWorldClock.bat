5e0a4961507c8b8b9982daa91238454805210b57
Nibras Hassan Nathu <nibrashassan11@gmail.com>
2025-12-18 14:41:27 +0100

    Add files via upload
    Manually added run file for the java code to execute java code in one click

---
+++ RunWorldClock.bat
@@ -0,0 +1,13 @@
+@echo off
+title NibrasH World Clock
+cd /d "C:\Users\nathu\IdeaProjects\NibrasHWorldClockProject"
+echo ========================================
+echo  NIBRASHASSAN WORLD CLOCK APPLICATION
+echo ========================================
+echo.
+echo Starting application...
+echo.
+java -jar NibrasHWorldClock.jar
+echo.
+echo Application closed.
+pause
